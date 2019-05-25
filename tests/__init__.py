a59d023bd482a86a8bae6619cf18559791a360a5
munichpavel <munichpavel@gmail.com>
2019-05-25 10:08:11 +0200

    Initial commit

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+"""Unit test package for clovek_ne_jezi_se."""
---
+++ tests/test_clovek_ne_jezi_se.py
@@ -0,0 +1,6 @@
+"""Tests for `clovek_ne_jezi_se` package."""
+
+import pytest
+
+
+from clovek_ne_jezi_se import clovek_ne_jezi_se
