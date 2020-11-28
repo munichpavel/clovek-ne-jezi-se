9b8dc51a109ac5b92647e2e63255415a0ec3d25a
munichpavel <munichpavel@gmail.com>
2020-11-28 11:11:49 +0100

    Change to readthedocs sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,9 @@
 #
 import os
 import sys
+
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../../'))
 
 
@@ -35,7 +38,8 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx_autodoc_typehints',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,7 +56,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
