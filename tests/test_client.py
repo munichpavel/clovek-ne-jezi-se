63945a900a0edc08fadd1d3c465caffbf0f6acca
munichpavel <munichpavel@gmail.com>
2020-12-30 12:27:28 +0100

    Organize client tests into class

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,43 +4,31 @@ from clovek_ne_jezi_se.agent import Player
 from clovek_ne_jezi_se.game_state import GameState
 
 
-def test_initial_state():
-    player_names = ['red', 'blue', 'green', 'yellow']
-    players = [Player(name=name) for name in player_names]
-
-    client = Client(players=players, main_board_section_length=4)
-    client.initialize()
-
-    expected_game_state = GameState(player_names, section_length=4)
-
-    assert client.get_game_state() == expected_game_state
+def monkey_roll(roll_value):
+    """Monkeypatch for dice roll"""
+    return roll_value
 
 
-def test_next_player():
+class TestClient:
     player_names = ['red', 'blue', 'green', 'yellow']
     players = [Player(name=name) for name in player_names]
 
     client = Client(players=players)
     client.initialize()
-    player_round = [
-        client.next_player() for _ in range(len(player_names))
-    ]
 
-    assert player_round == players
+    def test_initial_state(self):
+        expected_game_state = GameState(self.player_names)
 
+        assert self.client.get_game_state() == expected_game_state
 
-def monkey_roll(roll_value):
-    return roll_value
+    def test_next_player(self):
+        player_round = [
+            self.client.next_player() for _ in range(len(self.player_names))
+        ]
 
+        assert player_round == self.players
 
-def test_dice_roll_monkeypatch(monkeypatch):
-    player_names = ['red', 'blue', 'green', 'yellow']
-    players = [Player(name=name) for name in player_names]
-
-    client = Client(players=players, number_of_dice_faces=6)
-    client.initialize()
-    value = 1
-    monkeypatch.setattr(client, 'roll', lambda: monkey_roll(value))
-    assert client.roll() == value
-
-
+    def test_dice_roll_monkeypatch(self, monkeypatch):
+        value = 1
+        monkeypatch.setattr(self.client, 'roll', lambda: monkey_roll(value))
+        assert self.client.roll() == value
