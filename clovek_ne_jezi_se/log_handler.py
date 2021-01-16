595b3311731ba00fbb68dc537f49593938be96ec
munichpavel <munichpavel@gmail.com>
2021-01-16 22:15:01 +0100

    Flake8 up log_handler

--- clovek_ne_jezi_se/log_handler.py
+++ clovek_ne_jezi_se/log_handler.py
@@ -1,10 +1,9 @@
 import sys
 import logging
 
-#logger = logging.getLogger("clovek-ne-jezi-se")
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s:%(name)s:%(levelname)s:%(message)s'
+)
 handler.setFormatter(formatter)
-#logger.addHandler(handler)
-#logger.propagate = True
