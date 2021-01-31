90b753ca3be173df664500b712b2c04d76f776bb
munichpavel <munichpavel@gmail.com>
2021-01-31 12:14:43 +0100

    Add experiment querying example

--- scripts/play.py
+++ scripts/play.py
@@ -5,23 +5,22 @@ from mlflow.tracking import MlflowClient
 from clovek_ne_jezi_se.client import Client
 from clovek_ne_jezi_se.agents import HumanPlayer, RandomPlayer
 
+# TODO put in config file (json)
 player_names = ['red', 'blue', 'green', 'yellow']
 
 random_players = [
     RandomPlayer(name=name, print_game_state=False) for name in player_names
 ]
-
 players = random_players
 
-
-n_runs = 99
+n_runs = 96
 main_board_section_length = 1
 pieces_per_player = 4
 number_of_dice_faces = 6
 agents = [player.__class__.__name__ for player in players]
 
 run_dict = dict(
-    agents=str(agents),
+    agents=','.join(agents),
     main_board_section_length=main_board_section_length,
     pieces_per_player=pieces_per_player,
     number_of_dice_faces=number_of_dice_faces
